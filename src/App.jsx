38b298d2ceeab5bea73dec4e9627d61437390a57
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-12-07 16:57:31 +0700

    feat: add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/blog/:id" element={<Blog />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
